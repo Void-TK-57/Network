972b3f60b27756b98facc9f0c09e50533a048d30
Void <pedroaa57@gmail.com>
2019-10-03 12:50:35 -0300

    Add Echo Implement

--- client.py
+++ client.py
@@ -1,1 +1,33 @@
-import twisted
+# import main libs
+from twisted.internet.protocol import Protocol, ReconnectingClientFactory
+from twisted.internet import reactor
+from sys import stdout
+
+# main basic echo protocol
+class Echo(Protocol):
+
+    def dataReceived(self, data):
+        print(data)
+
+class EchoClientFactory(ReconnectingClientFactory):
+
+    def startedConnecting(self, connector):
+        print('Started to connect.')
+
+    def buildProtocol(self, addr):
+        print('Connected.')
+        print('Resetting reconnection delay')
+        self.resetDelay()
+        return Echo()
+
+    def clientConnectionLost(self, connector, reason):
+        print('Lost connection.  Reason:', reason)
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print('Connection failed. Reason:', reason)
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
+
+# if executed
+if __name__ == "__main__":
+    reactor.connectTCP('localhost', 8057, EchoClientFactory())
--- server.py
+++ server.py
@@ -1,1 +1,35 @@
-import twisted
+# import main libraries
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Factory
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import reactor
+
+# creat a basic echo protocol
+class Echo(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        # increase the numbers of protocols connected of the factory
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        # initially write to the connetion the initial message
+        self.transport.write( "Welcome! There are currently %d open connections.\n" % (self.factory.numProtocols,)) 
+
+    def connectionLost(self, reason):
+        # decrease the numbers of protocols connected of the factory
+        self.factory.numProtocols = self.factory.numProtocols - 1
+
+    def dataReceived(self, data):
+        # write the data back (raw)
+        self.transport.write(data)
+
+# main factory
+class EchoFactory(Factory):
+
+    def buildProtocol(self, addr):
+        return Echo(self)
+
+# 8007 is the port you want to run under. Choose something >1024
+reactor.listenTCP(8057, EchoFactory())
+reactor.run()
